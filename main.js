f24216f19854ef23dddb24410b802ee850b42a22
Ismail <ismailmuhammed2019@gmail.com>
2021-06-15 16:08:48 +0100

    added bottom message

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let count = 0;
+let countb = 0
 
 let score = document.querySelector('.scorea');
+let scoreb = document.querySelector('.scoreb');
 let btns = document.querySelectorAll('.btn');
+let warning = document.querySelector('#warning');
 
 btns.forEach(btn => {
 
@@ -12,10 +15,34 @@ btns.forEach(btn => {
             
         }else if(styles.contains('increase')){
             count++;
+        }else if (styles.contains('decreaseb')){
+            countb--;
+        }else if (styles.contains('increaseb')){
+            countb++;
         }else {
             count = 0;
+            countb = 0;
         }
         score.textContent = count;
+        scoreb.textContent = countb;
+
+        if((count < 0)){
+            score.style.color = "red";
+            warning.textContent = "You are losing the Game"
+        }else if((count > 0)){
+            score.style.color = "green";
+            warning.textContent = "You are winning the Game"
+        }else if(countb < 0){
+            scoreb.style.color= "red";
+            warning.textContent = "You are losing the Game"
+        }else if (countb > 0){
+            scoreb.style.color = "green"
+            warning.textContent = "You are winning the Game"
+        }
+        else{
+            score.style.color = "white";
+            scoreb.style.color= "white";
+        }
     }
     )
     
