edd45d2e5e255a45e97cf2f25adebd0387e98ecb
muyi2905 <obaremimuyiwa@gmail.com>
2023-09-10 00:13:12 +0000

    added new variable to keep track of max score

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let count = 0;
-let countb = 0
+let playerScore = 0;
+let opponentScore = 0;
+let maxScore = 100;
 
 let score = document.querySelector('.scorea');
 let scoreb = document.querySelector('.scoreb');
@@ -7,46 +8,37 @@ let btns = document.querySelectorAll('.btn');
 let warning = document.querySelector('#warning');
 let warning1 = document.querySelector('#warning1');
 
-btns.forEach(btn => {
+function clickHandler(e) {
+    let btn = e.currentTarget;
 
-    btn.addEventListener('click', (e) => {
-        let styles = e.currentTarget.classList;
-        if(styles.contains('decrease')){
-            count--;
-            
-        }else if(styles.contains('increase')){
-            count++;
-        }else if (styles.contains('decreaseb')){
-            countb--;
-        }else if (styles.contains('increaseb')){
-            countb++;
-        }else {
-            count = 0;
-            countb = 0;
-            warning.textContent = " ";
-            warning1.textContent = " ";
-        }
-        score.textContent = count;
-        scoreb.textContent = countb;
-
-        if((count < 0)){
-            score.style.color = "red";
-            warning.textContent = "You are losing the Game"
-        }else if((count > 0)){
-            score.style.color = "green";
-            warning.textContent = "You are winning the Game"
-        }else if(countb < 0){
-            scoreb.style.color= "red";
-            warning1.textContent = "You are losing the Game"
-        }else if (countb > 0){
-            scoreb.style.color = "green"
-            warning1.textContent = "You are winning the Game"
-        }
-        else{
-            score.style.color = "grey";
-            scoreb.style.color= "grey";
-        }
+    if (btn.classList.contains('decrease')) {
+        playerScore -= 1;
+    } else if (btn.classList.contains('increase')) {
+        playerScore += 1;
+    } else if (btn.classList.contains('decreaseb')) {
+        opponentScore -= 1;
+    } else if (btn.classList.contains('increaseb')) {
+        opponentScore += 1;
     }
-    )
-    
+
+    if (playerScore == maxScore) {
+        alert("You won!");
+    } else if (opponentScore == maxScore) {
+        alert("You lost!");
+    }
+
+    score.textContent = playerScore;
+    scoreb.textContent = opponentScore;
+}
+
+btns.forEach(btn => {
+    btn.addEventListener('click', clickHandler);
 });
+
+window.onload = function () {
+    // Reset the score when the page loads.
+    playerScore = 0;
+    opponentScore = 0;
+    score.textContent = playerScore;
+    scoreb.textContent = opponentScore;
+};
